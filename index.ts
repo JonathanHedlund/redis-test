90b39a7216626f38f58081492eab60f0f44e3c95
Jonathan <jonathan.hedlund.371@gmail.com>
2023-11-02 15:51:01 +0100

    messy implementation of caching photos and photo endpoints

--- index.ts
+++ index.ts
@@ -1,61 +1,72 @@
 import express from "express";
 import cors from "cors";
-import Redis from "redis";
-import util from "util";
+import Redis, { RedisClientType } from "redis";
 
-const redisClient = Redis.createClient();
+const redisClient = Redis.createClient() as RedisClientType;
 
 const DEFAULT_EXPIRATION = 3600;
 
-const getAsync = util.promisify(redisClient.get).bind(redisClient);
-
 await redisClient.connect();
 
-const redisMiddleware = async (
-	req: express.Request,
-	res: express.Response,
-	next: express.NextFunction
-) => {
-	try {
-		const cachedData = await redisClient.get(
-			`photos?albumId=${req.query.albumId}`
-		);
-		if (cachedData !== null) {
-			console.log("Cashe hit");
-			res.send(JSON.parse(cachedData));
-		} else {
-			next();
+const cacheCheck =
+	(redisClient: RedisClientType, key: string, queryParam?: string) =>
+	async (
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction
+	) => {
+		const params = req.params.id || "";
+		try {
+			const cacheKey = `${key}${
+				queryParam ? `_query-${queryParam}:${req.query[`${queryParam}`]}` : ""
+			}${params ? `_params:${params}` : ""}`;
+			const cachedData = await redisClient.get(cacheKey);
+			if (cachedData !== null) {
+				console.log("Cashe Hit");
+				res.send(JSON.parse(cachedData));
+			} else {
+				next();
+			}
+		} catch (error) {
+			res.send(error);
 		}
-	} catch (error) {
-		res.send(error);
-	}
-};
+	};
 
 const app = express();
 
 app.use(cors());
 
-app.get("/photos", redisMiddleware, async (req, res) => {
-	console.log("Cashe Miss");
-	const albumId = req.query.albumId;
-	const response = await fetch(
-		`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`
-	);
-	const photos = await response.json();
-	redisClient.setEx(
-		`photos?albumId=${albumId}`,
-		DEFAULT_EXPIRATION,
-		JSON.stringify(photos)
-	);
-	res.json(photos);
-});
+app.get(
+	"/photos",
+	cacheCheck(redisClient, `photos`, "albumId"),
+	async (req, res) => {
+		console.log("Cashe Miss");
+		const albumId = req.query.albumId;
+		const response = await fetch(
+			`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`
+		);
+		const photos = await response.json();
+		redisClient.setEx(
+			`photos_query-albumId:${albumId}`,
+			DEFAULT_EXPIRATION,
+			JSON.stringify(photos)
+		);
+		res.json(photos);
+	}
+);
 
-app.get("/photos/:id", async (req, res) => {
+app.get("/photos/:id", cacheCheck(redisClient, "photo"), async (req, res) => {
+	console.log("Cashe Miss");
 	const id = req.params.id;
 	const response = await fetch(
 		`https://jsonplaceholder.typicode.com/photos/${id}`
 	);
 	const photo = await response.json();
+	redisClient.setEx(
+		`photo_params:${id}`,
+		DEFAULT_EXPIRATION,
+		JSON.stringify(photo)
+	);
 	res.json(photo);
 });
 
